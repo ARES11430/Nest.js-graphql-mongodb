f55aee82110f96c7bdd97f7a234b76acf67c0caa
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-11-22 19:49:04 +0330

    Create Student Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
+import { LessonModule } from './lesson/lesson.module';
+import { Student } from './student/student.entity';
 import { StudentModule } from './student/student.module';
 
 @Module({
@@ -12,7 +13,7 @@ import { StudentModule } from './student/student.module';
       type: 'mongodb',
       url: 'mongodb://localhost/school',
       synchronize: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
